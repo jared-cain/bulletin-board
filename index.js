eafa95bd80a1ffe23fd3c405131a4e80d07fe53d
Jared Cain <jared.cain77@gmail.com>
2016-10-10 17:54:02 -0700

    Include openshift variables to app listen

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 5000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.use(express.static("./"));
 
-app.listen(port);
+app.listen(server_port, server_ip_address);
 
 console.log(`Bulletin app running on port ${port}`);
